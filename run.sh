d4616bf7bde3b5d09ea09f44584297444af0060b
Rafael Pezzi <rafael.pezzi@cern.ch>
2020-03-15 14:51:13 -0300

    Refining run.sh to deal with existing and running containers

--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+LOCALDIR=$HOME/alidocklite # shared dir on the host
+WORKDIR=/home/alidocklite  # shared dir inside the container
+NAME=alidocklite
 
-# WORKDIR will be shared between the docker container and the host machine.
-# Be sure to have this folder owned by $USER.
-LOCALDIR=$HOME/alidocklite_ubuntu18.04
-WORKDIR=/home/alidocklite
+ALIDOCKLITE_RUN_CMD="docker run -d -it -p 22222:22 --hostname $NAME --name $NAME -e HOSTUSER=$USER  -v $LOCALDIR:$WORKDIR alidocklite_ubuntu18.04"
 
-echo docker run -it -v $LOCALDIR:$WORKDIR alidocklite_ubuntu18.04
-docker run -it -v $LOCALDIR:$WORKDIR alidocklite_ubuntu18.04
+ALIDOCKLITE_ENTER_CMD="ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -XC 0.0.0.0 -p 22222 -i $LOCALDIR/.alidock-ssh/alidock.pem"
+
+ALIDOCKLITE_RM_CMD="docker rm $NAME"
+
+$ALIDOCKLITE_RUN_CMD &> /dev/null
+if [ $? == 0 ];
+   then
+   sleep .5
+   $ALIDOCKLITE_ENTER_CMD
+   else
+   $ALIDOCKLITE_RM_CMD &> /dev/null
+   $ALIDOCKLITE_RUN_CMD &> /dev/null
+   sleep .5
+   $ALIDOCKLITE_ENTER_CMD
+fi
